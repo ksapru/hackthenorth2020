fd01b7ee0c80b286ce2a8e38756774771628a93f
rdjauhari <r.djauhari99@gmail.com>
2021-01-15 23:07:58 -0800

    Videofeed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import MyNavbar from "../src/components/my-navbar/my-navbar.component";
+import Interview from './pages/Interview';
+
 
 function App() {
   return (
     <div className="App">
       <MyNavbar/>
+      <Interview/>
     </div>
   );
 }
--- src/pages/Interview.js
+++ src/pages/Interview.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Webcam from "react-webcam"
 
 export default function Interview() {
+    const videoConstraints = {
+        width: 575,
+        height: 375,
+        mirrored: true,
+        facingMode: "user" //front-facing camera
+    };
+
     return (
         <div>
-            
+            <Webcam
+                style={{ //centering the videofeed
+                    position: 'absolute', left: '50%', top: '57%',
+                    transform: 'translate(-50%, -50%)'
+                }}
+                audio={true}
+                videoConstraints={videoConstraints}
+            />
         </div>
     )
 }
