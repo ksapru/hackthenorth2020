f80ff4f1ae41178401b58410e96ba00e0dfd6712
Michael Chang <mchang0926@gmail.com>
2021-01-16 23:58:52 -0800

    update frontend

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,11 +1,11 @@
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import lightbulb from '../assets/img/lightbulb.svg';
+// import lightbulb from '../assets/img/lightbulb.svg';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid'
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Divider from '@material-ui/core/Divider';
-import { Line } from 'react-chartjs-2';
+// import CircularProgress from '@material-ui/core/CircularProgress';
+// import Divider from '@material-ui/core/Divider';
+// import { Line } from 'react-chartjs-2';
 
 
 
